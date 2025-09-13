31ef87276af97782505a09d0020673d8f89c1d6d
guest271314 <guest271314@gmail.com>
2025-09-13 06:03:45 +0000

    Update wt-client.js

--- wt-client.js
+++ wt-client.js
@@ -1,4 +1,12 @@
-(async () => {
+/*
+  ~/firefox/firefox-bin -headless -P wt wt-client.html | grep WEBTRANSPORT_CLIENT
+  ~/chrome-linux/chrome --headless --enable-logging=stderr --disable-gpu --password-store=basic wt-client.html 2>&1 | grep WEBTRANSPORT_CLIENT
+  node --no-warnings wt-client.js
+  bun --no-warnings wt-client.js
+  DENO_COMPAT=1 deno -A wt-client.js
+*/
+;(async () => {
+  globalThis.WT_CLIENT = `WEBTRANSPORT_CLIENT: `;
   const CERT_DIGEST = new Uint8Array();
   try {
     const serverCertificateHashes = [
@@ -22,7 +30,7 @@
     );
 
     await client.ready;
-    console.log("WebTransport client ready");
+    console.log(`${WT_CLIENT}WebTransport client ready.`);
 
     const { readable, writable } = await client.createBidirectionalStream();
     let data = new Uint8Array(1024 ** 2 * 20);
@@ -35,33 +43,48 @@
     ));
     let view = new DataView(header.buffer);
     let outgoingTotalLength = view.getUint32(0, true);
-    console.log({ outgoingTotalLength });
+      console.log(`${WT_CLIENT}${JSON.stringify({outgoingTotalLength})}`);
     let incomingTotalLength = 0;
     const writer = writable.getWriter();
     await writer.ready;
     await writer.write(header).then(() =>
-      console.log(`Outgoing total length ${outgoingTotalLength} written.`)
+      console.log(
+        `${WT_CLIENT}Outgoing total length ${outgoingTotalLength} written.`,
+      )
     );
     await writer.ready;
+
     await writer.write(data)
-      .then(() => console.log(`${data.length} bytes written.`)).catch((e) =>
-        console.log(e)
-      );
+      .then(() => console.log(`${WT_CLIENT}${data.length} bytes written.`))
+      .catch((e) => {
+        console.log(`${WT_CLIENT}${e.message}`);
+      });
+
     await writer.ready;
     await writer.close();
     for await (const value of readable) {
       incomingTotalLength += value.length;
       if (incomingTotalLength === outgoingTotalLength) {
-        console.log({ incomingTotalLength, outgoingTotalLength });
+        console.log(
+          `${WT_CLIENT}${
+            JSON.stringify({ incomingTotalLength, outgoingTotalLength })
+          }`,
+        );
         break;
       }
     }
     client.close({ closeCode: 4999, reason: "Done streaming." });
-    await client.closed
-      .then(console.log).catch(console.warn);
+    return await client.closed
+      .then((result) => console.log(`${WT_CLIENT}${JSON.stringify(result)}`))
+      .catch((e) => {
+        console.log(`${WT_CLIENT}${e.message}`);
+      });
   } catch (e) {
-    console.log(e);
+    console.log(`${WT_CLIENT}${e.message}`);
   }
-})().catch((e) => {
-  console.log(e);
-});
+})()
+  .catch((e) => {
+    console.log(`${WT_CLIENT}${e.message}`);
+  })
+  .then(() => console.log(`${WT_CLIENT}${navigator.userAgent} closing.`))
+  .finally(() => globalThis?.window?.close());
